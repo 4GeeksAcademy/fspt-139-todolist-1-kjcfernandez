152a351381d133e6f10d067a7fa53f1f3102eaf7
kjcfernandez <kelvin.cunalema@gmail.com>
2026-07-18 11:16:29 +0000

    finalizado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [inputValue, setInputValue] = useState("")
+	const [tareas, setTareas] = useState([])
+	const eliminarTarea = (index) => {
+		const nuevasTareas = tareas.filter((_,i) => i !== index)
+		setTareas(nuevasTareas)
+	}
+	// const esValido = inputValue.trim().length >= 3
+	// console.log(esValido);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	return (
+		<div className="page">
+			<h1 className="title">To do</h1>
+			<div className="card">
+			<input
+				className="input"
+				type="text"
+				placeholder="Enter your action..."
+				value={inputValue}
+				onChange={e => setInputValue(e.target.value)}
+				onKeyUp={e => {
+					if (e.key === "Enter" && inputValue.trim().length > 0) {
+						setTareas([...tareas, inputValue])
+						setInputValue("")
+					}
+				}} />
+			<ul className="list">
+				{
+					tareas.map((tarea, index) => {
+						return (
+							<li key={index} className="item">
+								<span>{tarea}</span>
+								<span
+								className="delete-icon"
+								onClick={() => eliminarTarea(index)}>
+									x
+								</span>
+							</li>
+						)
+					})
+				}
+			</ul>
+			{
+				tareas.length > 0 &&
+				<div className="footer">
+					{tareas.length} item{tareas.length !== 1 ? "s" :""} left
+				</div>
+			}
+			{/* {
+				!esValido &&
+				<p>El texto debe ser de al menos 3 caracteres</p>
+			} */}
+			</div>
 		</div>
 	);
 };
